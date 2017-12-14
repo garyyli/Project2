db554b904297328511fce61657336b760c30919c
garyyli <garyli@hanovernorwichschools.org>
2017-12-14 14:38:28 -0500

    Updated from Brython Server: 12/14/2017 2:38:28 PM

--- battleship.py
+++ battleship.py
@@ -1,3 +1,14 @@
 #Gary Li
 #12/14/17
 #battleship.py
+
+board = [['O','O','O'],['O','O','O'],['O','O','O']]
+
+def buildBoard():
+    for row in range(0,3):
+        for col in range(0,3):
+            print(board[row][col],' ',end = '')
+        print()
+
+buildBoard()
+
