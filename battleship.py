e7c9f3632d05ae8f76ba7471a659e88939346dc5
garyyli <garyli@hanovernorwichschools.org>
2017-12-14 14:46:39 -0500

    Updated from Brython Server: 12/14/2017 2:46:39 PM

--- battleship.py
+++ battleship.py
@@ -2,11 +2,11 @@
 #12/14/17
 #battleship.py
 
-board = [['O','O','O'],['O','O','O'],['O','O','O']]
+board = [['O','O','O','O','O'],['O','O','O','O','O'],['O','O','O','O','O'],['O','O','O','O','O'],['O','O','O','O','O']]
 
 def buildBoard():
-    for row in range(0,3):
-        for col in range(0,3):
+    for row in range(0,5):
+        for col in range(0,5):
             print(board[row][col],' ',end = '')
         print()
 
