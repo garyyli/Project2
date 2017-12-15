9dbb0b01377be9474459b9dd5fa2a57577443388
garyyli <garyli@hanovernorwichschools.org>
2017-12-15 14:29:47 -0500

    Updated from Brython Server: 12/15/2017 2:29:48 PM

--- battleship.py
+++ battleship.py
@@ -2,13 +2,18 @@
 #12/14/17
 #battleship.py
 
+from random import randint
 board = [['O','O','O','O','O'],['O','O','O','O','O'],['O','O','O','O','O'],['O','O','O','O','O'],['O','O','O','O','O']]
 
-def buildBoard():
+def buildBoard1():
     for row in range(0,5):
         for col in range(0,5):
             print(board[row][col],' ',end = '')
         print()
 
-buildBoard()
+buildBoard1()
 
+
+def redrawAll:
+    for item in App().spritelist[:]:
+        item.destroy()
