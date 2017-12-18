8ac75df64089038f27c46089df03a053022d96ea
garyyli <garyli@hanovernorwichschools.org>
2017-12-18 14:22:20 -0500

    Updated from Brython Server: 12/18/2017 2:22:19 PM

--- battleship.py
+++ battleship.py
@@ -13,17 +13,27 @@ def buildBoard():
 buildBoard()
 
 
-def redrawAll:
+def redrawAll():
     for item in App().spritelist[:]:
         item.destroy()
     for row in range(0,5):
         for col in range(0,5):
             print(board[row][col],' ',end = '')
         
-if __name__ == '__main__': 
-
-white = Color(0xFFFFFF,1)
-red = Color(0xFF0000,1)
-black = Color(0x000000,1)
-
-App().run()
+if __name__ == '__main__':
+    data = {}
+    
+    black = Color(0x000000,1)
+    white = Color(0xFFFFFF,1)
+    blue = Color(0x0000FF, 1)
+    red = Color(0xFF0000,1)
+    
+    playerBoard = buildBoard()
+    computerBoard = buildBoard()
+    
+    emptyCircle = CircleAsset(radius,LineStyle(1,black),white)
+    missCircle = CircleAsset(radius,LineStyle(1,black), blue)
+    hitCircle = CircleAsset(radius,LineStyle(1,black),red)
+    
+    
+    App().run()
