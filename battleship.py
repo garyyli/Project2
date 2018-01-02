8b9df5a173d51482696a0c3cf7787ce06718f0a9
garyyli <garyli@hanovernorwichschools.org>
2018-01-02 14:31:38 -0500

    Updated from Brython Server: 1/2/2018 2:31:37 PM

--- battleship.py
+++ battleship.py
@@ -18,7 +18,7 @@ def redrawAll():
         item.destroy()
     for row in range(0,5):
         for col in range(0,5):
-            Sprite(emptyCircle,(radius*2*row+RADIUS, radius*2*col+RADIUS))
+            Sprite(emptyCircle,(radius*2*row+radius, radius*2*col+radius))
 
 def mouseClick(event):
     print(event.x,event.y)
