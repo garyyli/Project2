7153c1509a6ca74b1e5e826d3eb6e29a416ef518
garyyli <garyli@hanovernorwichschools.org>
2018-01-03 19:26:14 -0500

    Updated from Brython Server: 1/3/2018 7:26:14 PM

--- battleship.py
+++ battleship.py
@@ -43,13 +43,13 @@ def redrawAll():
 
 def mouseClick(event):
     print(event.x,event.y)
-    if data['playerShips'] <= 3:
-        if event.x <= radius*5 and event.y <= radius*5:
-                playerRow = event.y//(radius*2)
-                playerCol = event.x//(radius*2)
-                if data['playerBoard'][playerRow][playerCol] != Ships:
-                    data['playerShips'] += 0
-                redrawAll()
+    if event.x <= radius*5 and event.y <= radius*5:
+            playerRow = event.y//(radius*2)
+            playerCol = event.x//(radius*2)
+            if data['playerBoard'][playerRow][playerCol] != Ships:
+                data['playerShips'] += 0
+            redrawAll()
+    
                     
 
         
