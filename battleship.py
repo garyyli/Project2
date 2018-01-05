2334dfa8b9fba06237e478634d94b01f07171c76
garyyli <garyli@hanovernorwichschools.org>
2018-01-04 22:32:14 -0500

    Updated from Brython Server: 1/4/2018 10:32:14 PM

--- battleship.py
+++ battleship.py
@@ -90,7 +90,8 @@ if __name__ == '__main__':
     hitCircle = CircleAsset(radius, blackLine,red)
     shipCircle = CircleAsset(radius, blackLine, grey)
     textPlayer = TextAsset('Player', fill=black,style='bold 30pt Times')
-    textComputer = TextAsset('Computer', fill=black,style='bold 30pt Times')
+    textPlayerWins = TextAsset('Player wins!!', fill=black,style='bold 40pt Times')
+    textComputerWins = TextAsset('Computer wins!!', fill=black,style='bold 40pt Times')
     
     redrawAll()
 
